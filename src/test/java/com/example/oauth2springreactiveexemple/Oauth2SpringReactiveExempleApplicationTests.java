05712a401bfa5926464e3daca64297a7a980fbac
ludovic landschoot <ludovic.landschoot@ext.leroymerlin.fr>
2019-10-17 16:56:20 +0200

    chore (init): first commit

---
+++ src/test/java/com/example/oauth2springreactiveexemple/Oauth2SpringReactiveExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2springreactiveexemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SpringReactiveExempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
