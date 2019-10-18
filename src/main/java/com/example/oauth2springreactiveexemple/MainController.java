c1e76d0bd68a02db06bd7a9eee9dec11699611cf
ludovic landschoot <ludovic.landschoot@ext.leroymerlin.fr>
2019-10-18 17:17:30 +0200

    feat (security): add configuration oauth2 and main controller

---
+++ src/main/java/com/example/oauth2springreactiveexemple/MainController.java
@@ -0,0 +1,23 @@
+package com.example.oauth2springreactiveexemple;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/authorize")
+    public Mono<String> index(@AuthenticationPrincipal Mono<OAuth2User> oauth2User) {
+        return oauth2User
+                .map(OAuth2User::getName)
+                .map(name -> String.format("Hi, %s", name));
+    }
+
+    @GetMapping("/about")
+    public String getAboutPage() {
+        return "WebFlux OAuth example";
+    }
+}
